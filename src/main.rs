11d01c0555050cc76f0541ff42ac8c4f232a5a5f
Bernhard Schuster <bernhard@ahoi.io>
2024-07-10 14:56:55 +0200

    x

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,16 @@ enum Command {
 async fn main() -> Result<()> {
     color_eyre::install()?;
     let args = <Args as clap::Parser>::try_parse()?;
-    let pd = directories::ProjectDirs::from("", "io.konifay", "prda").expect("Has home dir. qed");
+    let Args { cmd, config, } = args;
     
-    let config = pd.config_dir().join("config.toml");
-    let config = fs::read_to_string(config)?;
+    let config_path = if let Some(config_path) = config {
+        config_path
+    } else {
+        let pd = directories::ProjectDirs::from("", "io.konifay", "prda").expect("Has home dir. qed");
+        let config_path = pd.config_dir().join("config.toml");
+        config_path            
+    };
+    let config = fs::read_to_string(config_path)?;
     let config: Config = toml::from_str(config.as_str())?;
     
     let repo = gix::open(std::env::current_dir()?)?;
@@ -51,23 +57,45 @@ async fn main() -> Result<()> {
     //     bail!("Cannot work with bare repositories");
     // }
     
-    dbg!(repo.head_commit()?);
-    dbg!(repo.head_ref()?);
-    dbg!(repo.head_name()?);
-    repo.head()?;
-    dbg!(repo.head_id()?);
+    match cmd {
+        Command::Pr { title, branch, base } => {
+            
+            let base = base.as_ref().map(|s| s.as_str()).unwrap_or_else(|| config.default_base.as_ref().map(|s| s.as_str()).unwrap_or("base"));
+            dbg!(repo.head_commit()?);
+            let Some(head_name) = repo.head_name()?else {
+                bail!("Must be on a named branch, otherwise this won't work")
+            };
+            let branch = head_name.shorten();
 
-    let title = "title";
+            let mut iter = repo.remote_names().into_iter().filter_map(|remote| repo.try_find_remote(&*remote));
+            let (owner, repo) = loop {
+                let Some(remote) = iter.next() else {
+                    bail!("Didn't find a remote to deduce owner and repo from..")
+                };
+                let remote = remote?;
+                let Some(remote) = remote.url(gix::remote::Direction::Push) else {
+                    bail!("Failed to find remote (push) url");
+                };
+                let re = regex::Regex::new(r#"^(git@|https://)github.com[:/](?<owner>[a-zA-z0-9]+)/(?<repo>[a-zA-z0-9]+)(\.git)?$"#).expect("Regex is sane. qed");
+                let haystack = remote.to_string();
+                if let Some(captures) = re.captures(&haystack) {
+                    let owner = captures.name("owner").expect("Always present. qed");
+                    let repo = captures.name("repo").expect("Always present. qed");
+                    break dbg!((owner.as_str().to_owned(), repo.as_str().to_owned()));
+                }
+            };
+
+            let oc = octocrab::OctocrabBuilder::default().personal_token(config.token).set_connect_timeout(None).build()?;
+            let prh = oc.pulls(owner, repo);
+            let pr = prh.create(title, dbg!(branch.to_string()), dbg!(base)).body("foo").maintainer_can_modify(true).draft(true).send().await?;
+            let PullRequest { number, url, id, .. } = pr;
+            println!("{number}");
+
+        }
+         _ => todo!()
+    }
     let branch = "foo";
     let base = "main";
-    let owner = "drahnr";
-    let repo = "whatever";
-    // let (owner, repo) = remote.parse();
-    let oc = octocrab::OctocrabBuilder::default().personal_token(config.token).set_connect_timeout(None).build()?;
-    let prh = oc.pulls(owner, repo);
-    let pr = prh.create(title, branch, base).body("foo").maintainer_can_modify(true).draft(true).send().await?;
-    let PullRequest { number, url, id, .. } = pr;
-    println!("{number}");
     
     Ok(())
 }
